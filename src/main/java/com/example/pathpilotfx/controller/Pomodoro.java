69e25c00eb74a9cce737f5034cf06602f3a7b5e4
Pablo-Flores-QUT <132873316+Pablo-Flores-QUT@users.noreply.github.com>
2024-04-21 20:25:20 +1000

    Changed variable names and added some comment for clarity

--- src/main/java/com/example/pathpilotfx/controller/Pomodoro.java
+++ src/main/java/com/example/pathpilotfx/controller/Pomodoro.java
@@ -4,13 +4,13 @@ import javafx.animation.Timeline;
 
 public class Pomodoro {
     private int seconds;
-    private String display;
-    private int sessionCount;
+    private String display; // text string formatted to represent the timer display.
+    private int sessionCount; // sessionCount to be used for long breaks.
     private int rest;
     private int work;
     private Boolean isWork; //work = true, rest = false
 
-
+    //constructor for default 25min - 5min timer
     public Pomodoro () {
         this.work = 25;
         this.seconds = 1500; //25 * 60
@@ -20,6 +20,8 @@ public class Pomodoro {
         this.isWork = true;
 
     }
+
+    //constructor for customised timer duration
     public Pomodoro (int work, int rest) {
         this.work = work;
         this.seconds = work * 60;
@@ -37,10 +39,13 @@ public class Pomodoro {
         return display;
     }
 
+    // creates a timeline for enabling the timer animation.
     public Timeline setTimeline() {
         return new Timeline();
     }
 
+    //decrements the seconds count by 1, and updates displays.
+    //this method will be used for the keyframe animation to update the timer every 1 second.
     public void decreaseSeconds() {
         seconds--;
         display = String.format("%02d:%02d", seconds/60 , seconds % 60);
@@ -51,6 +56,7 @@ public class Pomodoro {
         this.display = String.format("%02d:%02d", seconds/60 , seconds % 60);
     }
 
+    //changes the session type and returns a string representing the current type
     public String toggleType(){
         isWork = !isWork;
         if(isWork) {return "FOCUS";} else return "BREAK";
--- src/main/java/com/example/pathpilotfx/controller/TimerController.java
+++ src/main/java/com/example/pathpilotfx/controller/TimerController.java
@@ -4,58 +4,56 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.util.Duration;
 
-import java.util.Objects;
-
 public class TimerController {
     @FXML
-    private Pomodoro focusTimer;
+    private Pomodoro sessionTimer;
     @FXML
-    private Timeline pomodoroTimeline;
+    private Timeline timerTimeline;
 
     @FXML
-    private Label timer;
+    private Label timerDisplay;
     @FXML
-    private Label type;
+    private Label timerType;
 
     @FXML
     protected void onStartButtonClick() {
-        pomodoroTimeline.play();
+        timerTimeline.play();
     }
 
     @FXML
     protected void onStopButtonClick() {
-        pomodoroTimeline.stop();
-        focusTimer.resetTimer();
-        timer.setText(focusTimer.getDisplay());
+        timerTimeline.stop();
+        sessionTimer.resetTimer();
+        timerDisplay.setText(sessionTimer.getDisplay());
     }
 
     @FXML
     protected void onTypeButtonClick(){
-        type.setText(focusTimer.toggleType());
-        focusTimer.resetTimer();
-        timer.setText(focusTimer.getDisplay());
+        timerTimeline.stop();
+        timerType.setText(sessionTimer.toggleType());
+        sessionTimer.resetTimer();
+        timerDisplay.setText(sessionTimer.getDisplay());
     }
 
 
     @FXML
     public void initialize(){
-        focusTimer = new Pomodoro();
-        timer.setText(focusTimer.getDisplay());
-        pomodoroTimeline = focusTimer.setTimeline();
-        pomodoroTimeline.getKeyFrames().addAll(new KeyFrame(Duration.seconds(1), (ActionEvent event) -> {
-            focusTimer.decreaseSeconds();
-            timer.setText(focusTimer.getDisplay());
-            if(focusTimer.getSeconds()==0) {
-                pomodoroTimeline.stop();
-                type.setText(focusTimer.toggleType());
-                focusTimer.resetTimer();
-                timer.setText(focusTimer.getDisplay());
+        sessionTimer = new Pomodoro();
+        timerDisplay.setText(sessionTimer.getDisplay());
+        timerTimeline = sessionTimer.setTimeline();
+        timerTimeline.getKeyFrames().addAll(new KeyFrame(Duration.seconds(1), (ActionEvent event) -> {
+            sessionTimer.decreaseSeconds();
+            timerDisplay.setText(sessionTimer.getDisplay());
+            if(sessionTimer.getSeconds()==0) {
+                timerTimeline.stop();
+                timerType.setText(sessionTimer.toggleType());
+                sessionTimer.resetTimer();
+                timerDisplay.setText(sessionTimer.getDisplay());
             }
         }));
-        pomodoroTimeline.setCycleCount(Timeline.INDEFINITE);
+        timerTimeline.setCycleCount(Timeline.INDEFINITE);
     }
 }
